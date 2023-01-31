86f2838e82b618fa36dfdaece718cd12602385d7
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-01-30 18:27:21 -0800

    Basic Menu Work + TMP
    - Created Basic Menu
    - Added prefabs folder
    - Imported TMP and TMP examples/extras

---
+++ DetectiveGame/Assets/Menu_Manager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_Manager : MonoBehaviour
+{
+    public GameObject AllMenus;
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+    public GameObject CreditsMenu;
+
+    private GameObject CurrentMenu;
+    private GameObject NextMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+        CreditsMenu.SetActive(false);
+        CurrentMenu = MainMenu;
+    }
+
+    public void startGame()
+    {
+        AllMenus.SetActive(false);
+        // code here to start the game
+    }
+
+    public void QuitGame()
+    {
+        // debug statement for in-editor functionality checking
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
