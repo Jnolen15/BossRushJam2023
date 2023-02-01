6fe459e36b91a380a204afa0b49e7f6c6d94acd5
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-01-31 20:37:49 -0800

    Finish mouseover functionality
    - Menus now pop to the front when you mouse over their tab
    - added a highlight to communicate when menu tabs are moused over

--- DetectiveGame/Assets/Moused_Over.cs
+++ DetectiveGame/Assets/Moused_Over.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-public class Moused_Over : MonoBehaviour, IPointerEnterHandler
+public class Moused_Over : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
-    public GameObject MenuFolder;
-
+    public GameObject Highlight;
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        Debug.Log("mousedOver");
+        // Debug.Log("mousedOver");
+        this.transform.parent.transform.parent.SetAsLastSibling();
+        // ^this is currently just changing the order of the children of the main menu
+
+        Highlight.SetActive(true);
     }
 
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Highlight.SetActive(false);
+    }
 }
