e794929e808a5c48712e31f539cdbcedf338e52e
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-02-13 20:32:20 -0800

    Fixed Menu Transition, added new scene to menu scene
    - Camera transition broke tho

--- DetectiveGame/Assets/Menu_Manager.cs
+++ DetectiveGame/Assets/Menu_Manager.cs
@@ -7,25 +7,36 @@ using UnityEngine.SceneManagement;
 
 public class Menu_Manager : MonoBehaviour
 {
+    // public
     public GameObject MainMenu;
     public GameObject SettingsMenu;
     public GameObject CreditsMenu;
+    public Transform CameraTable;
+    public Transform CameraNormal;
 
+    // private
     private GameObject CurrentMenu;
     private GameObject NextMenu;
-    // Start is called before the first frame update
+
     void Start()
     {
         MainMenu.SetActive(true);
         SettingsMenu.SetActive(true);
         CreditsMenu.SetActive(true);
         CurrentMenu = MainMenu;
+
+        Camera.main.transform.position = CameraTable.position;
     }
 
     public void startGame()
     {
         this.gameObject.SetActive(false);
         // code here to animate looking up from desk
+
+        Camera.main.transform.position = CameraNormal.position;
+        Camera.main.transform.rotation = CameraNormal.transform.rotation;
+
+        // load next scene when done
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -34,5 +45,17 @@ public class Menu_Manager : MonoBehaviour
         // debug statement for in-editor functionality checking
         Debug.Log("Quitting Game");
         Application.Quit();
+    }
+
+    IEnumerator CameraTransition()
+    {
+        var speed = 1f;
+        while (Camera.main.transform.position != CameraNormal.position)
+        {
+            Camera.main.transform.position = Vector3.Lerp(CameraTable.position, CameraNormal.position, speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        } 
+        // load next scene when done
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
