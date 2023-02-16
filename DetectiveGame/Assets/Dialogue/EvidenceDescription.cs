58a091a32d961e2fde97c0d93b03dde128478eed
KapeRay <67844957+KapeRay@users.noreply.github.com>
2023-02-15 19:48:07 -0800

    Dialogue finish for level 1 please god

---
+++ DetectiveGame/Assets/Dialogue/EvidenceDescription.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EvidenceDescription : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string evidence;
+    public string description;
+    public GameObject evidenceWindow;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseOver()
+    {
+        
+    }
+}
