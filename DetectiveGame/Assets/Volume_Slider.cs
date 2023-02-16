00b9b5618e9c3dfd885718f93f5c8fd22e03e409
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-02-15 19:37:17 -0800

    Revert "Hooked volume mixers"
    This reverts commit e94991d892a015a8aac69a77d2093e69bae3972c.

--- DetectiveGame/Assets/Volume_Slider.cs
+++ DetectiveGame/Assets/Volume_Slider.cs
@@ -11,7 +11,7 @@ public class Volume_Slider : MonoBehaviour
     [SerializeField] AudioMixer _mixer;
     [SerializeField] Slider _slider;
     [SerializeField] float _multiplier = 30f;
-    [SerializeField] float sliderValue = 0.8f;
+    [SerializeField] float sliderValue = 1;
 
     private void Awake()
     {
