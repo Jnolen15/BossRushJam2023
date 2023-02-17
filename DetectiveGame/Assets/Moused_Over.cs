4257f73da9841435297ab9ed6d60bc81b3f33289
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-02-16 18:47:59 -0800

    Folder covers now close automatically

--- DetectiveGame/Assets/Moused_Over.cs
+++ DetectiveGame/Assets/Moused_Over.cs
@@ -8,13 +8,17 @@ public class Moused_Over : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 {
     public GameObject Highlight;
     public GameObject FolderCover;
+    public bool FolderOn = true;
+    public bool FolderOpenCall = true;
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        this.GetComponent<AudioSource>().Play();
+        this.GetComponent<AudioSource>().Play(); 
 
         // Debug.Log("mousedOver");
         this.transform.parent.transform.parent.SetAsLastSibling();
         FolderCover.SetActive(false);
+        FolderOn = false;
+        FolderOpenCall = true;
 
         Highlight.SetActive(true);
     }
@@ -22,5 +26,20 @@ public class Moused_Over : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         Highlight.SetActive(false);
+    }
+
+    public void CloseFolder()
+    {
+        FolderCover.SetActive(true);
+        FolderOn = true;
+    }
+
+    public void Update()
+    {
+        if (this.transform.parent.transform.parent.GetSiblingIndex() < 3)
+        {
+            FolderCover.SetActive(true);
+            FolderOn = true;
+        }
     }
 }
